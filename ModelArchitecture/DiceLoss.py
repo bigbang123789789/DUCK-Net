15b903cbf967ba624e6abbf807402e8b18e522bb
bigbang123789789 <134249895+bigbang123789789@users.noreply.github.com>
2025-03-23 18:13:36 +0700

    Update DiceLoss.py

--- ModelArchitecture/DiceLoss.py
+++ ModelArchitecture/DiceLoss.py
@@ -1,14 +1,14 @@
-import keras.backend as K
 import tensorflow as tf
 
-
 def dice_metric_loss(ground_truth, predictions, smooth=1e-6):
-    ground_truth = K.cast(ground_truth, tf.float32)
-    predictions = K.cast(predictions, tf.float32)
-    ground_truth = K.flatten(ground_truth)
-    predictions = K.flatten(predictions)
-    intersection = K.sum(predictions * ground_truth)
-    union = K.sum(predictions) + K.sum(ground_truth)
+    ground_truth = tf.cast(ground_truth, tf.float32)
+    predictions = tf.cast(predictions, tf.float32)
+    
+    ground_truth = tf.reshape(ground_truth, [-1])  # Flatten
+    predictions = tf.reshape(predictions, [-1])    # Flatten
+
+    intersection = tf.reduce_sum(predictions * ground_truth)
+    union = tf.reduce_sum(predictions) + tf.reduce_sum(ground_truth)
 
     dice = (2. * intersection + smooth) / (union + smooth)
 
